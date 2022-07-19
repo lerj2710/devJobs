a873b6cabfda3d55e6b6d5b1190b3416303fde1f
Luis Rodriguez <kike.lerj2710@gmail.com>
2022-07-19 08:14:21 -0400

    env

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const router = require('./routes');
-const exphbs = require('express-handlebars');
+require("dotenv").config();
 
-const path = require('path');
+const express = require("express");
+const router = require("./routes");
+const exphbs = require("express-handlebars");
+
+const path = require("path");
 const app = express();
-const port = 5000
 
-app.use('/', router());
+app.use("/", router());
 
 // habilitar handlebars como view
-app.engine('handlebars', exphbs.engine({
-    defaultLayout: 'layout',
-    layoutsDir: path.join(app.get('views'), 'layouts')
-}));
+app.engine(
+  "handlebars",
+  exphbs.engine({
+    defaultLayout: "layout",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+  })
+);
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 // static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //correr el puerto
-app.listen(port, () => {
-  console.log(`Corriendo en el Puerto ${port}`)
-})
+app.listen(process.env.port, () => {
+  console.log(`corriedo en el puerto ${process.env.port}`);
+});
