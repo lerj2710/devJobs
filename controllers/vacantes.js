b2900f7d37cb5f5d9890912e30e3a1a566efbde2
Luis Rodriguez <kike.lerj2710@gmail.com>
2022-07-21 20:03:06 -0400

    se agrego nueva-vacante hbs

--- controllers/vacantes.js
+++ controllers/vacantes.js
@@ -1,5 +1,8 @@
 const formularioNuevaVacante = (req, res) => {
-  res.send("funciona");
+  res.render("nueva-vacante", {
+    nombrePagina: "Nueva Vacante",
+    tagline: "Llena el formulario y publica tu vacante",
+  });
 };
 
 module.exports = {
